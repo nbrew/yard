efad8adc687f7346027340bf191e09f5ada7a117
Nathan Hyde <nhyde@mbayaq.org>
2015-05-08 18:45:02 -0700

    Add reader,writer,accessor attributes to sample fixture.

--- spec/fixtures/sample.rb
+++ spec/fixtures/sample.rb
@@ -1,4 +1,9 @@
 class UndocumentedClass
+
+  attr_writer :write_only
+  attr_reader :read_me
+  attr_accessor :read_write
+
   def undocumented_method(param1, param2=3)
     'The method is not documented!'
   end
